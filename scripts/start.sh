705cf959d0f5404ef1b545c82094c14ef6095152
jsjsjskjs <jsjsjskjs@gmail.com>
2021-10-27 18:21:34 +0900

    deploy step 2

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 cd /home/ubuntu/AtoZsports/server
 
+export COOKIE_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names COOKIE_SECRET --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_USER=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PORT --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PORT --query Parameters[0].Value | sed 's/"//g')
-
+export GMAIL_ID=$(aws ssm get-parameters --region ap-northeast-2 --names GMAIL_ID --query Parameters[0].Value | sed 's/"//g')
+export GMAIL_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names GMAIL_PASSWORD --query Parameters[0].Value | sed 's/"//g')
+export NODE_ENV=$(aws ssm get-parameters --region ap-northeast-2 --names NODE_ENV --query Parameters[0].Value | sed 's/"//g')
 
 authbind --deep pm2 start app.js
