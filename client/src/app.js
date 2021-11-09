b81546f90d735869615c2c9c790953896f44c8f3
steel-hyuk <skypeed3@naver.com>
2021-11-09 13:19:09 +0900

    just add

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import GlobalStyle from './globalStyle/globalStyle'
